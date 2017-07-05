d2f694cb101482ed9449cc088e24a95770ddb1ce
xingxiaogang <xingxiaogang@corp.apus.com>
2017-07-05 19:11:57 +0800

    add:添加indicator

--- app/src/main/java/com/example/xingxiaogang/animationdemo/MainActivity.java
+++ app/src/main/java/com/example/xingxiaogang/animationdemo/MainActivity.java
@@ -22,9 +22,11 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.WindowManager;
 import android.widget.BaseAdapter;
+import android.widget.CheckedTextView;
 import android.widget.FrameLayout;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.xingxiaogang.animationdemo.view.ColorDotLoadingDrawable;
@@ -39,6 +41,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private RadarView iconScanView;
     private ViewGroup mRootView;
     private String TAG = "TEST_";
+    public static int rowCount = 2;
+    public static int columns = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -120,8 +124,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    class Dialog extends android.app.Dialog {
-        List<ResolveInfo> intentInfos = new ArrayList<>();
+    class Dialog extends android.app.Dialog implements ViewPager.OnPageChangeListener {
+        private List<ResolveInfo> intentInfos = new ArrayList<>();
+        private List<CheckedTextView> mIndicators = new ArrayList<>();
+        private LinearLayout mIndicatorContainer;
+        boolean isUpdatedHeight;
 
         public Dialog(@NonNull Context context, int style) {
             super(context, style);
@@ -130,7 +137,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            View item = View.inflate(MainActivity.this, R.layout.pop_bottom_menu, null);
+            final View item = View.inflate(MainActivity.this, R.layout.pop_bottom_menu, null);
+            mIndicatorContainer = (LinearLayout) item.findViewById(R.id.dot_container);
 
             //加载数据 todo 后台
             loadShareInfos();
@@ -139,11 +147,21 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             getWindow().setGravity(Gravity.BOTTOM);
             getWindow().setContentView(item);
-            WindowManager.LayoutParams lp = getWindow().getAttributes();
-            DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
-            lp.width = metrics.widthPixels;
-            lp.height = (int) (metrics.heightPixels * 1.0f / 5 * 2);
-            getWindow().setAttributes(lp);
+
+//            item.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+//                @Override
+//                public void onGlobalLayout() {
+//
+//                    WindowManager.LayoutParams lp = getWindow().getAttributes();
+//                    DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+//                    lp.width = metrics.widthPixels;
+//                    lp.height = item.getMeasuredHeight();
+//                    getWindow().setAttributes(lp);
+//
+//                    item.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+//                }
+//            });
+
             setCanceledOnTouchOutside(true);
         }
 
@@ -164,151 +182,210 @@ public class MainActivity extends Activity implements View.OnClickListener {
             }
         }
 
-        void bindView(ViewGroup view, List<ResolveInfo> intentInfos) {
-            ViewPager pager = (ViewPager) view.findViewById(R.id.view_pager);
-            pager.setAdapter(new MenuPageAdapter(intentInfos));
-        }
-    }
+        void bindView(ViewGroup menuView, List<ResolveInfo> intentInfos) {
+            ViewPager pager = (ViewPager) menuView.findViewById(R.id.view_pager);
+            MenuPageAdapter adapter = new MenuPageAdapter(intentInfos);
+            pager.setAdapter(adapter);
+            pager.addOnPageChangeListener(this);
 
-    public static int rowCount = 2;
-    public static int columns = 3;
-
-    //页
-    class MenuPageAdapter extends PagerAdapter {
-
-        private List<ResolveInfo> mAllInfos = new ArrayList<>();
-
-
-        public MenuPageAdapter(List<ResolveInfo> intentInfos) {
-            //todo 动态计算行列
-            mAllInfos.addAll(intentInfos);
-        }
-
-        @Override
-        public int getCount() {
-            return Math.round(mAllInfos.size() * 1.0f / (rowCount * columns));
-        }
-
-        @Override
-        public boolean isViewFromObject(View view, Object object) {
-            return view == object;
-        }
-
-        @Override
-        public Object instantiateItem(ViewGroup container, int position) {
-            MenuPage page = new MenuPage(container.getContext(), getInfosForPage(position));
-            container.addView(page);
-            return page;
-        }
-
-        @Override
-        public void destroyItem(ViewGroup container, int position, Object object) {
-            ((MenuPage) object).destroy(container);
-            object = null;
-
-        }
-
-        private List<ResolveInfo> getInfosForPage(int page) {
-            int pageSize = rowCount * columns;
-            List<ResolveInfo> infos = new ArrayList<>();
-            for (int position = pageSize * page; position < pageSize * (page + 1) && position < mAllInfos.size(); position++) {
-                infos.add(mAllInfos.get(position));
+            //指示器
+            int pageCount = adapter.getCount();
+            int size = SizeUtils.dp2px(5);
+            if (pageCount > 1) {
+                for (int i = 0; i < pageCount; i++) {
+                    CheckedTextView dot = new CheckedTextView(menuView.getContext());
+                    dot.setBackgroundResource(R.drawable.indicator_selector);
+                    LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(size, size);
+                    lp.leftMargin = size;
+                    lp.rightMargin = size;
+                    mIndicatorContainer.addView(dot, lp);
+                    mIndicators.add(dot);
+                }
+                updateDots(0);
             }
-            Log.d(TAG, "getInfosForPage: page=" + page + " size=" + infos.size() + " all size=" + mAllInfos.size());
-            return infos;
-        }
-    }
 
-
-    class MenuPage extends FrameLayout {
-
-        GridView mGridView;
-
-        public MenuPage(@NonNull Context context, List<ResolveInfo> infos) {
-            super(context);
-            inflate(context, R.layout.grid_layout, this);
-            mGridView = (GridView) findViewById(R.id.grid_view);
-            mGridView.setNumColumns(columns);
-            int width = Resources.getSystem().getDisplayMetrics().widthPixels / columns;
-            mGridView.setColumnWidth(width);
-            mGridView.setVerticalSpacing(SizeUtils.dp2px(8));
-            Adapter adapter = new Adapter(infos);
-            mGridView.setAdapter(adapter);
-            adapter.notifyDataSetChanged();
         }
 
-        void destroy(ViewGroup parent) {
-            if (getParent() == parent) {
-                parent.removeView(this);
+        private void updateDots(int position) {
+            if (mIndicators.size() > 1) {
+                for (int i = 0; i < mIndicators.size(); i++) {
+                    CheckedTextView checkble = mIndicators.get(i);
+                    checkble.setChecked(position == i);
+                }
             }
         }
 
-        class Adapter extends BaseAdapter {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-            private List<ResolveInfo> mPageInfos;
+        }
 
-            public Adapter(@NonNull List<ResolveInfo> pageInfos) {
-                this.mPageInfos = pageInfos;
+        @Override
+        public void onPageSelected(int position) {
+            updateDots(position);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+
+        //页
+        class MenuPageAdapter extends PagerAdapter {
+
+            private List<ResolveInfo> mAllInfos = new ArrayList<>();
+
+
+            public MenuPageAdapter(List<ResolveInfo> intentInfos) {
+                mAllInfos.addAll(intentInfos);
             }
 
             @Override
             public int getCount() {
-                return mPageInfos.size();
+                return Math.round(mAllInfos.size() * 1.0f / (rowCount * columns));
             }
 
             @Override
-            public ResolveInfo getItem(int position) {
-                return mPageInfos.get(position);
+            public boolean isViewFromObject(View view, Object object) {
+                return view == object;
             }
 
             @Override
-            public long getItemId(int position) {
-                return position;
+            public Object instantiateItem(ViewGroup container, int position) {
+                MenuPage page = new MenuPage(container.getContext(), getInfosForPage(position));
+                ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+                container.addView(page, lp);
+                return page;
             }
 
             @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                Holder holder = null;
-                if (convertView == null) {
-                    convertView = View.inflate(parent.getContext(), R.layout.grid_item, null);
-                    holder = new Holder(convertView);
-                } else {
-                    holder = (Holder) convertView.getTag();
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                ((MenuPage) object).destroy(container);
+                object = null;
+
+            }
+
+            private List<ResolveInfo> getInfosForPage(int page) {
+                int pageSize = rowCount * columns;
+                List<ResolveInfo> infos = new ArrayList<>();
+                for (int position = pageSize * page; position < pageSize * (page + 1) && position < mAllInfos.size(); position++) {
+                    infos.add(mAllInfos.get(position));
                 }
-                convertView.setTag(holder);
-
-                holder.bind(convertView.getContext(), getItem(position));
-
-                return convertView;
+                Log.d(TAG, "getInfosForPage: page=" + page + " size=" + infos.size() + " all size=" + mAllInfos.size());
+                return infos;
             }
         }
 
-        class Holder {
-            ImageView mImageView;
-            TextView mTextView;
+        class MenuPage extends FrameLayout {
 
-            Holder(View itemView) {
-                mImageView = (ImageView) itemView.findViewById(R.id.grid_img);
-                mTextView = (TextView) itemView.findViewById(R.id.grid_txt);
+            GridView mGridView;
+
+            public MenuPage(@NonNull Context context, List<ResolveInfo> infos) {
+                super(context);
+                final View child = View.inflate(context, R.layout.grid_layout, null);
+                addView(child, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+                //测量内容高度，以给viewpager设置
+                if (!isUpdatedHeight) {
+                    child.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                        @Override
+                        public void onGlobalLayout() {
+                            int childHeight = child.getMeasuredHeight();
+                            Log.d(TAG, "onGlobalLayout: " + childHeight);
+                            //更新窗口高度
+                            WindowManager.LayoutParams lp = getWindow().getAttributes();
+                            DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+                            lp.width = metrics.widthPixels;
+                            lp.height = childHeight + SizeUtils.dp2px(35);
+                            getWindow().setAttributes(lp);
+
+                            child.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                        }
+                    });
+                    isUpdatedHeight = true;
+                }
+                mGridView = (GridView) findViewById(R.id.grid_view);
+                mGridView.setNumColumns(columns);
+                int width = Resources.getSystem().getDisplayMetrics().widthPixels / columns;
+                mGridView.setColumnWidth(width);
+                mGridView.setVerticalSpacing(SizeUtils.dp2px(8));
+                Adapter adapter = new Adapter(infos);
+                mGridView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
             }
 
-
-            public void bind(Context context, Object item) {
-                ResolveInfo info = ((ResolveInfo) item);
-                if (TextUtils.equals(info.activityInfo.targetActivity, "edit") && TextUtils.equals(info.activityInfo.packageName, "setting")) {
-                    mImageView.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.notification_icon));
-                } else {
-                    mImageView.setImageDrawable(info.loadIcon(context.getPackageManager()));
+            void destroy(ViewGroup parent) {
+                if (getParent() == parent) {
+                    parent.removeView(this);
                 }
-                String l = "";
-                CharSequence label = info.loadLabel(context.getPackageManager());
-                if (label != null) {
-                    l = label.toString().replaceAll("\\s*", "");
-                }
-                mTextView.setText(l);
             }
+
+            class Adapter extends BaseAdapter {
+
+                private List<ResolveInfo> mPageInfos;
+
+                public Adapter(@NonNull List<ResolveInfo> pageInfos) {
+                    this.mPageInfos = pageInfos;
+                }
+
+                @Override
+                public int getCount() {
+                    return mPageInfos.size();
+                }
+
+                @Override
+                public ResolveInfo getItem(int position) {
+                    return mPageInfos.get(position);
+                }
+
+                @Override
+                public long getItemId(int position) {
+                    return position;
+                }
+
+                @Override
+                public View getView(int position, View convertView, ViewGroup parent) {
+                    Holder holder = null;
+                    if (convertView == null) {
+                        convertView = View.inflate(parent.getContext(), R.layout.grid_item, null);
+                        holder = new Holder(convertView);
+                    } else {
+                        holder = (Holder) convertView.getTag();
+                    }
+                    convertView.setTag(holder);
+
+                    holder.bind(convertView.getContext(), getItem(position));
+
+                    return convertView;
+                }
+            }
+
+            class Holder {
+                ImageView mImageView;
+                TextView mTextView;
+
+                Holder(View itemView) {
+                    mImageView = (ImageView) itemView.findViewById(R.id.grid_img);
+                    mTextView = (TextView) itemView.findViewById(R.id.grid_txt);
+                }
+
+
+                public void bind(Context context, Object item) {
+                    ResolveInfo info = ((ResolveInfo) item);
+                    if (TextUtils.equals(info.activityInfo.targetActivity, "edit") && TextUtils.equals(info.activityInfo.packageName, "setting")) {
+                        mImageView.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.notification_icon));
+                    } else {
+                        mImageView.setImageDrawable(info.loadIcon(context.getPackageManager()));
+                    }
+                    String l = "";
+                    CharSequence label = info.loadLabel(context.getPackageManager());
+                    if (label != null) {
+                        l = label.toString().replaceAll("\\s*", "");
+                    }
+                    mTextView.setText(l);
+                }
+            }
+
         }
-
     }
 
 
