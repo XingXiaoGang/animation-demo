449ec52e504688e836d9eaf84accc6695c70c95e
xingxiaogang <xingxiaogang@corp.apus.com>
2017-07-19 15:39:14 +0800

    add:actionMenu添加label

--- app/src/main/java/com/example/xingxiaogang/animationdemo/ActionMenuActivity.java
+++ app/src/main/java/com/example/xingxiaogang/animationdemo/ActionMenuActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by xingxiaogang on 2017/7/18.
@@ -11,15 +12,28 @@ import android.view.View;
 
 public class ActionMenuActivity extends Activity implements View.OnClickListener {
 
+    IMenu mActionMenu;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.action_menu_layout);
+        mActionMenu = (IMenu) findViewById(R.id.action_menu);
     }
 
 
     @Override
     public void onClick(View v) {
-        ((IMenu) findViewById(R.id.action_menu)).toggle();
+        switch (v.getId()) {
+            case R.id.menu_handle: {
+                mActionMenu.toggle();
+                break;
+            }
+            default: {
+                mActionMenu.close();
+                Toast.makeText(this, v.getContentDescription().toString(), Toast.LENGTH_LONG).show();
+                break;
+            }
+        }
     }
 }
